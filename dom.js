67eb830922bdd7061f3bc04df8b290a0fd1149cd
arun kumar <arunkumarpostbox2369@gmail.com>
2022-08-25 20:31:09 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,17 @@
+//console.dir(document);
+//console.log(document.title);
+//console.log(document.URL);
+//console.log(document.head)
+//console.log(document.all);
+//document.all[10].textContent= 'Hello';
+//console.log(document.doctype);
+//document.all[10].textContent= "Hello";
+var headerTitle=document.getElementById('header-title');
+console.log(headerTitle);
+headerTitle.textContent='Hello';
+headerTitle.style.borderBottom='solid 3px #000';
+var header = document.getElementById('main-header');
+console.log('headerTitle');
+var listgroupitem = document.getElementById("list-group-item");
+
+TextData.textContent = "<b> item1 </b>";
